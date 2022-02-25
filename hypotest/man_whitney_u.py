a0b274beff50576190d03d5d14b5ed3f0a82889c
Leslie <60702562+les1smore@users.noreply.github.com>
2022-02-25 12:18:32 -0500

    Mann Whitney U test

--- hypotest/man_whitney_u.py
+++ hypotest/man_whitney_u.py
@@ -1,10 +1,13 @@
+# Prepare the data
 test_team = np.array([6.2, 7.1, 1.5, 2,3 , 2, 1.5, 6.1, 2.4, 2.3, 12.4, 1.8, 5.3, 3.1, 9.4, 2.3, 4.1])
 developer_team = ([2.3, 2.1, 1.4, 2.0, 8.7, 2.2, 3.1, 4.2, 3.6, 2.5, 3.1, 6.2, 12.1, 3.9, 2.2, 1.2 ,3.4])
 
+# Assumption check
 check_normality(test_team)
 check_normality(developer_team)
 check_variance_homogeneity(test_team, developer_team)
 
+# Select the proper test
 ttest, pvalue = stats.mannwhitneyu(test_team, developer_team, alternative = 'two-sided')
 print('p-value:%.4f' % pvalue)
 if pvalue < 0.05:
